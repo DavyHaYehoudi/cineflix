e60cd939316b700a4a83cadcb15666c1a6a8e9a9
Davy SEBBAH <david@alonactiv.com>
2022-05-06 21:42:05 +0300

    mise en forme des cards + sort + moteur de recherche

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,63 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Form = () => {
 
-    useEffect(()=>{
-        axios.get('https://api.themoviedb.org/3/movie/550?api_key=bdca27504590d572df21083a2fdf78ab&query=star&language=fr-FR')
-        .then(data => console.log(data))
-    }, [])
-    
+    const [moviesData, setMoviesData] = useState([]);
+    const [search, setSearch] = useState('code');
+    const [sortGoodBad, setSortGoodBad] = useState(null)
 
-    // const[movie,setMovie]=useState([]);
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}&language=fr-FR`)
+            .then(res => setMoviesData(res.data.results))
+        // .then(res =>console.log(res))
+    }, [search])
 
-    // const handle = (e) =>{
-    //     e.preventDefault();
-    //     axios('https://api.themoviedb.org/3/movie/550?api_key=bdca27504590d572df21083a2fdf78ab')
-    //         .then(res => setMovie(res))
-    //         .catch(err => console.log(err))
-    // }
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
 
     return (
         <div className='form-component'>
             <div className='form-container'>
                 <form>
                     <input type='text'
-                           placeholder="Entrez le titre d'un film"
-                           id='search-input' />
+                        placeholder="Entrez le titre d'un film"
+                        id='search-input'
+                        onChange={(e) => handleSearch(e)} />
 
-                    <input type='submit' 
-                           value='Rechercher' />
+                    <input type='submit'
+                        value='Rechercher' />
                 </form>
 
                 <div className='btn-sort-container'>
-                    <div className='btn-sort' id='goodToBad'>Top<span>➡️</span></div>
-                    <div className='btn-sort' id='badToGood'>Flop<span>➡️</span></div>
+                    <div className='btn-sort'
+                        id='goodToBad'
+                        onClick={() =>  setSortGoodBad('goodToBad')} >Top<span>➡️</span></div>
+                    <div className='btn-sort'
+                        id='badToGood'
+                        onClick={() => setSortGoodBad('BadToGood')} >Flop<span>➡️</span></div>
                 </div>
             </div>
-            {/* <div className='result'>{movie}</div> */}
+            <div className='result'>
+                {
+                    moviesData
+                        .slice(0, 12)
+                        .sort((a, b) => {
+                            if (sortGoodBad === 'goodToBad') {
+                                return b.vote_average - a.vote_average
+                            } else if (sortGoodBad === 'badToGood') {
+                                return a.vote_average - b.vote_average
+                            }
+                        })
+                        .map(movie =>
+                            <Card key={movie.id} movie={movie} />
+
+                        )
+                }
+            </div>
         </div>
     );
 };
